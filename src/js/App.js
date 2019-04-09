89d91068bf55a714aae73898754148c55ec51d94
Mnho229 <michaelho3624@gmail.com>
2019-04-09 17:39:10 -0400

    updated readme and finished basic card

--- src/js/App.js
+++ src/js/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import '../sass/App.sass';
+import example from '../images/example.jpg'
+
+class SocialCard extends React.PureComponent {
+	render() {
+		return (
+			<div className="sc-contain">
+				<img className="sc-img" alt="example" src={this.props.image} />
+				<div className="sc-desc">
+					<h4 className="sc-desc-h">{this.props.descH}</h4>
+					<p className="sc-desc-p">{this.props.descP}</p>
+				</div>
+			</div>
+		);
+	}
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			img1: example,
+			descH1: "The Example of the Decades",
+			descP1: "Bear witness to the growing century of the new world!  Come check it out today!"
+		}
+	}
+	render() {
+		return (
+			<div className="App">
+				<h1>Regular Social Card</h1>
+				<SocialCard image={this.state.img1} descH={this.state.descH1} descP={this.state.descP1} />
+				<h1>Weather Cards</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
